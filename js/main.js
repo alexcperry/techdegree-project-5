464cf109c3e9bcd4c17906407eee426942cdf62d
Alexander C Perry <acp96@cornell.edu>
2020-05-30 13:33:00 -0400

    fixed hover border bug

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+
+$('search-box').on('keyup', function () {
+
+  let searchTerm = $('search-box').val();
+
+  console.log(searchTerm);
+
+  // $('a').each(function(index,element) {
+
+  // });
+});
